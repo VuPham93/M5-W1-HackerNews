6a935d0bb3e66e207e7897c08975c924e6927021
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-08-11 11:18:09 +0700

    Add Like

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'M5-W1-HackerNews'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('M5-W1-HackerNews');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
